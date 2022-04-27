ea9b1c5233fbf11779118fc0407281ba559c6029
Soheil Solhjoo <46877039+soheilsolhjoo@users.noreply.github.com>
2022-04-27 12:19:03 +0200

    Add files via upload
    This is a test file to use p5.js for tracking mouse.

---
+++ 001.js
@@ -0,0 +1,26 @@
+var fill_color = 0;
+var color_change = 1;
+var radius = 50;
+
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+}
+
+function draw() {
+  circle(mouseX, mouseY, radius);
+  noStroke()
+  
+  fill_color += color_change;
+  if (fill_color > 255 || fill_color < 0) {
+    color_change = -color_change;
+  }
+  
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(fill_color,10);
+  }
+  
+  
+}
